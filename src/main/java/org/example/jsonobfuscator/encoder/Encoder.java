b44ee67452573c782813acf2d7d798fbe0ce8c1c
Eivydas Senkus <eivydas.senkus@gmail.com>
2020-11-15 18:56:49 +0200

    chore(documentation): add documentation to base interfaces

--- src/main/java/org/example/jsonobfuscator/encoder/Encoder.java
+++ src/main/java/org/example/jsonobfuscator/encoder/Encoder.java
@@ -6,5 +6,14 @@ import java.io.OutputStream;
 import java.util.Map;
 
 public interface Encoder {
+
+  /**
+   * Encodes input file
+   *
+   * @param inputStream data of file to encode
+   * @param outputStream data of encoded file
+   * @return encoded values and their equivalent code
+   * @throws IOException if reading/writing to streams failed
+   */
   Map<String, String> encode(InputStream inputStream, OutputStream outputStream) throws IOException;
 }
--- src/main/java/org/example/jsonobfuscator/reader/Reader.java
+++ src/main/java/org/example/jsonobfuscator/reader/Reader.java
@@ -3,7 +3,20 @@ package org.example.jsonobfuscator.reader;
 import java.io.IOException;
 
 public interface Reader {
+
+  /**
+   * checks if there are still bytes to read
+   *
+   * @return true if there are, false otherwise
+   */
   boolean hasText();
 
+  /**
+   * reads bytes until a specific character is encountered
+   *
+   * @param character character until which to read bytes
+   * @return all bytes until specified character
+   * @throws IOException exception if reading fails
+   */
   String readUntil(final char character) throws IOException;
 }
